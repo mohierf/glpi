19e5b1a237900f2e7bf2a127081a2a3bd7d00a40
Remi Collet <remi@remirepo.net>
2017-10-10 11:17:43 +0200

    CS

--- tools/cachebench.php
+++ tools/cachebench.php
@@ -16,22 +16,22 @@ $ent = new Entity();
 $nb = countElementsInTable('glpi_entities');
 if ($nb < 100000) {
    echo "+ Generate some entities\n";
-   for ($a = 0 ; $a < PER_LEVEL ; $a++) {
+   for ($a = 0; $a < PER_LEVEL; $a++) {
       $ida = $ent->add(['entities_id' => 0, 'name' => "ent $a"]);
       echo "$ida\r";
-      for ($b = 0 ; $b < PER_LEVEL ; $b++) {
+      for ($b = 0; $b < PER_LEVEL; $b++) {
          $idb = $ent->add(['entities_id' => $ida, 'name' => "s-ent $b"]);
          echo "$idb\r";
-         for ($c = 0 ; $c < PER_LEVEL ; $c++) {
+         for ($c = 0; $c < PER_LEVEL; $c++) {
             $idc = $ent->add(['entities_id' => $idb, 'name' => "ss-ent $c"]);
             //echo "$idc\r";
-            for ($d = 0 ; $d < PER_LEVEL ; $d++) {
+            for ($d = 0; $d < PER_LEVEL; $d++) {
                $idd = $ent->add(['entities_id' => $idc, 'name' => "sss-ent $d"]);
                //echo "$idd\r";
-               for ($e = 0 ; $e < PER_LEVEL ; $e++) {
+               for ($e = 0; $e < PER_LEVEL; $e++) {
                   $ide = $ent->add(['entities_id' => $idd, 'name' => "sss-ent $e"]);
                   //echo "$ide\r";
-                  for ($f = 0 ; $f < PER_LEVEL ; $f++) {
+                  for ($f = 0; $f < PER_LEVEL; $f++) {
                      $idf = $ent->add(['entities_id' => $ide, 'name' => "sss-ent $f"]);
                      //echo "$idf\r";
                   }
@@ -57,8 +57,10 @@ $DB->query("UPDATE glpi_entities SET sons_cache=NULL");
 
 $tps = microtime(true);
 echo "+ Run with empty cache\n";
-for ($i=0 ; $i<COUNT ; $i++) {
-   if (($i & 0x7f)==0) echo "$i\r";
+for ($i=0; $i<COUNT; $i++) {
+   if (($i & 0x7f)==0) {
+      echo "$i\r";
+   }
    $t[$i] = $id = mt_rand(0, $nb);
    $x = getSonsAndAncestorsOf('glpi_entities', $id);
 }
@@ -67,8 +69,10 @@ printf("> time: %.4f\n", $tps);
 
 $tps = microtime(true);
 echo "+ Run with populated cache\n";
-for ($i=0 ; $i<COUNT ; $i++) {
-   if (($i & 0x7f)==0) echo "$i\r";
+for ($i=0; $i<COUNT; $i++) {
+   if (($i & 0x7f)==0) {
+      echo "$i\r";
+   }
    $id = $t[$i];
    $x = getSonsAndAncestorsOf('glpi_entities', $id);
 }
